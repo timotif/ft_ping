4e5fae8920c018702e151a41018956011cabdc5c
Timoti Fregni <timotif@gmail.com>
2025-10-29 16:23:31 +0100

    - modular packet print

--- utils.c
+++ utils.c
@@ -6,19 +6,19 @@
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 10:58:49 by tfregni           #+#    #+#             */
-/*   Updated: 2025/10/28 16:38:01 by tfregni          ###   ########.fr       */
+/*   Updated: 2025/10/29 15:59:14 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ping.h"
 
-void	print_bytes(uint8_t *bytes, int len, char *header)
+void	print_bytes(uint8_t *bytes, size_t len, char *header)
 {
-	int	i;
+	size_t	i;
 
 	if (header)
 		printf("\n========== %s ==========\n", header);
-	printf("Total bytes: %d\n", len);
+	printf("Total bytes: %ld\n", len);
 	printf("Hex dump: ");
 	i = 0;
 	while (i < len){
@@ -29,85 +29,92 @@ void	print_bytes(uint8_t *bytes, int len, char *header)
 	printf("\n");
 }
 
-void	print_ip(uint8_t *buffer, u_int32_t total_bytes)
+void	print_ip(uint8_t *bytes, size_t len)
 {
 	t_ip_header		*ip;
-	t_icmp_header	*icmp;
 	size_t			ip_header_len;
-	size_t			i;
 
-	print_bytes(buffer, total_bytes, "RAW PACKET");
+	print_bytes(bytes, len, "RAW PACKET");
 	printf("\n\n========== IP HEADER ==========\n");
-	ip = (t_ip_header *)buffer;
+	ip = (t_ip_header *)bytes;
 	ip_header_len = ip->ihl * 4;
 	printf("Version:        %d\n", ip->version);
 	printf("IHL:            %d (%ld bytes)\n", ip->ihl, ip_header_len);
 	printf("TOS:            0x%02x\n", ip->tos);
 	printf("Total Length:   %d (wire: %02x %02x)\n",
-		ntohs(ip->tot_len), buffer[2], buffer[3]);
+		ntohs(ip->tot_len), bytes[2], bytes[3]);
 	printf("ID:             %d (wire: %02x %02x)\n",
-		ntohs(ip->id), buffer[4], buffer[5]);
+		ntohs(ip->id), bytes[4], bytes[5]);
 	printf("Frag offset:    %d (wire: %02x %02x)\n",
-		ntohs(ip->frag_off), buffer[6], buffer[7]);
+		ntohs(ip->frag_off), bytes[6], bytes[7]);
 	printf("TTL:            %d\n", ip->ttl);
 	printf("Protocol:       %d %s\n", ip->protocol,
 		ip->protocol == IPPROTO_ICMP ? "(ICMP)" : "");
 	printf("Checksum:       0x%04x (wire: %02x %02x)\n",
-		ntohs(ip->check), buffer[10], buffer[11]);
+		ntohs(ip->check), bytes[10], bytes[11]);
 	printf("Source IP:      %s (wire: %02x %02x %02x %02x)\n",
 		inet_ntoa(*(struct in_addr *)&ip->saddr),
-		buffer[12], buffer[13], buffer[14], buffer[15]);
+		bytes[12], bytes[13], bytes[14], bytes[15]);
 	printf("Dest IP:        %s (wire: %02x %02x %02x %02x)\n",
 		inet_ntoa(*(struct in_addr *)&ip->daddr),
-		buffer[16], buffer[17], buffer[18], buffer[19]);
-
-	if (ip->protocol == IPPROTO_ICMP && total_bytes > ip_header_len)
+		bytes[16], bytes[17], bytes[18], bytes[19]);
+	if (len <= ip_header_len)
+		return ;
+	switch (ip->protocol)
 	{
-		printf("\n========== ICMP HEADER ==========\n");
-		icmp = (t_icmp_header *)(buffer + ip_header_len);
-		printf("Type:           %d\n", icmp->type);
-		printf("Code:           %d\n", icmp->code);
-		printf("Checksum:       0x%04x (wire: %02x %02x)\n",
-			ntohs(icmp->checksum), buffer[ip_header_len + 2],
-			buffer[ip_header_len + 3]);
-		printf("ID:             %d (wire: %02x %02x)\n",
-			ntohs(icmp->un.echo.id), buffer[ip_header_len + 4],
-			buffer[ip_header_len + 5]);
-		printf("Sequence:       %d (wire: %02x %02x)\n",
-			ntohs(icmp->un.echo.sequence), buffer[ip_header_len + 6],
-			buffer[ip_header_len + 7]);
-
-		printf("\nICMP Payload (%ld bytes):\n",
-			total_bytes - ip_header_len - 8);
-		printf("          ");
-		for (i = ip_header_len + 8; i < total_bytes && i < ip_header_len + 24; i++)
-			printf("%02x ", buffer[i]);
-		if (total_bytes > ip_header_len + 24)
-			printf("...");
-		printf("\n");
+		case IPPROTO_ICMP:
+			print_icmp(bytes + ip_header_len, len - ip_header_len);
+			break;
+		default:
+			fprintf(stderr,"Unrecognized protocol: %d\n", ip->protocol);
 	}
 	printf("=====================================\n\n");
 }
-
-void	print_icmp(uint8_t *bytes, int len)
+	
+void	print_icmp(uint8_t *bytes, size_t len)
 {
-	int	i;
+	t_icmp_header	*icmp;
+	size_t			i;
+	
+	printf("\n========== ICMP HEADER ==========\n");
+	icmp = (t_icmp_header *)(bytes);
+	printf("Type:           %d\n", icmp->type);
+	printf("Code:           %d\n", icmp->code);
+	printf("Checksum:       0x%04x (wire: %02x %02x)\n",
+		ntohs(icmp->checksum), bytes[2],
+		bytes[3]);
+	printf("ID:             %d (wire: %02x %02x)\n",
+		ntohs(icmp->un.echo.id), bytes[4],
+		bytes[5]);
+	printf("Sequence:       %d (wire: %02x %02x)\n",
+		ntohs(icmp->un.echo.sequence), bytes[6],
+		bytes[7]);
 
-	i = 0;
-	while (i < 8)
-	{
-		if (i == 0)
-			printf("Type:\t");
-		if (i == 1)
-			printf("\nCode:\t");
-		if (i == 2)
-			printf("\nChksum:\t");
-		if (i == 4)
-			printf("\nRest:\t");
-		printf("%02x", (uint8_t) bytes[i++]);
-	}
-	printf("\nPayload:\t");
-	print_bytes(bytes + 8, len - 8, NULL);
+	printf("\nICMP Payload (%ld bytes):\n",
+		len - 8);
+	printf("          ");
+	for (i = 8; i < len && i < 24; i++)
+		printf("%02x ", bytes[i]);
+	if (len > 24)
+		printf("...");
+	printf("\n");
+	// int	i;
+
+	// i = 0;
+	// while (i < 8)
+	// {
+	// 	if (i == 0)
+	// 		printf("Type:\t");
+	// 	if (i == 1)
+	// 		printf("\nCode:\t");
+	// 	if (i == 2)
+	// 		printf("\nChksum:\t");
+	// 	if (i == 4)
+	// 		printf("\nRest:\t");
+	// 	printf("%02x", (uint8_t) bytes[i++]);
+	// }
+	// printf("\nPayload:\t");
+	// print_bytes(bytes + 8, len - 8, NULL);
 }
 
 void	print_usage(char *prog_name)
